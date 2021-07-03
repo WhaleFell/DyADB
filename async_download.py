531ffe608ece998f47497072a269f46d1e31eff2
AdminWhaleFall <2734184475@qq.com>
2021-07-04 00:29:42 +0800

    🐞 fix: 下载模块修复下载过快导致被封ip

--- async_download.py
+++ async_download.py
@@ -3,7 +3,7 @@
 '''
 Author: whalefall
 Date: 2021-07-03 21:13:26
-LastEditTime: 2021-07-03 23:20:23
+LastEditTime: 2021-07-04 00:18:09
 Description: 异步&多线程下载抖音无水印视频
 '''
 import asyncio
@@ -53,36 +53,31 @@ def do_load_media(video: tuple, path):
     path_full = os.path.join(path, "%s.mp4" % (file_name))
 
     try:
-        start_time = time.time()
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko)'}
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
         pre_content_length = 0
         # 循环接收视频数据
         while True:
             # 若文件已经存在，则断点续传，设置接收来需接收数据的位置
-            if os.path.exists(path_full):
-                headers['Range'] = 'bytes=%d-' % os.path.getsize(path_full)
+            if os.path.exists(path):
+                headers['Range'] = 'bytes=%d-' % os.path.getsize(path)
+            res = requests.get(url, stream=True, headers=headers)
 
-            res = requests.get(video_url, stream=True, headers=headers)
             content_length = int(res.headers['content-length'])
-
             # 若当前报文长度小于前次报文长度，或者已接收文件等于当前报文长度，则可以认为视频接收完成
-            if content_length < pre_content_length or (
-                    os.path.exists(path_full) and os.path.getsize(path_full) == content_length) or content_length == 0:
-                break
-
+            # if content_length < pre_content_length or (
+            #         os.path.exists(path) and os.path.getsize(path) == content_length) or content_length == 0:
+            #     break
             pre_content_length = content_length
 
             # 写入收到的视频数据
-            with open(path_full, 'ab') as file:
+            with open(path, 'ab') as file:
                 file.write(res.content)
                 file.flush()
-                end_time = time.time()
-                use_tiem = end_time-start_time
-                print('%s下载成功,用时:%s' %
-                      (path_full, use_tiem))
+                print('下载成功,file size:%d total size:%d' %
+                      (os.path.getsize(path), content_length))
     except Exception as e:
-        print("视频%s下载错误:%s" % (path_full, e))
+        print("出现错误:", e, video_url)
 
 
 def readSql(table_name) -> list:
@@ -133,9 +128,8 @@ def mkdirInPath(name):
 
 
 if __name__ == "__main__":
-    # 更改这个参数可以下载好多不一样的视频
-    table_name = "common"
 
+    table_name = "qinlanlan"
     mkdirInPath(table_name)
     # video_queue = queue.Queue()
     videoList = []
@@ -147,6 +141,9 @@ if __name__ == "__main__":
 
     print("一共有%s条视频,等待3s后下载" % (len(videoList)))
 
-    with ThreadPoolExecutor(max_workers=256) as pool2:
-        for video in videoList:
-            pool2.submit(do_load_media, video, path)
+    # with ThreadPoolExecutor(max_workers=64) as pool2:
+    #     for video in videoList:
+    #         # print(video)
+    #         pool2.submit(do_load_media, video, path)
+    for video in videoList:
+        do_load_media(video, path)
